713b7770ccf1c53e57c6c48c59d8d608fc223ed0
nelsonkct <nelsonkuo0430@gmail.com>
2025-04-22 22:15:50 +0800

    feat: change UI text

--- Server/HandleClient.py
+++ Server/HandleClient.py
@@ -12,63 +12,60 @@ class HandleClient:
         self.username = None
     def handleClient(self):
         try:
-            self.client.sendall(createMessage("info", "Welcome", False))
+            self.client.sendall(createMessage("info", "Welcome to Secure Data Matching Platform\n===================================", False))
             while True:
-                self.client.sendall(createMessage("info", "Enter R to Register L to login Q to quit",True))
+                self.client.sendall(createMessage("info", "Please select an option:\n[L] Login\n[R] Register\n[Q] Quit", True))
                 Letter = decodeMessage(self.client.recv(1024))
-                if Letter["data"] == 'L':
+                if Letter["data"].upper() == 'L':
                     if self.Login(): 
-                        mainSession = UserSession(self.client,self.username)
+                        mainSession = UserSession(self.client, self.username)
                         mainSession.run()
                         del mainSession
-                elif Letter["data"] == 'R':
+                elif Letter["data"].upper() == 'R':
                    self.Register()
-                elif Letter["data"] == 'Q':
-                    self.client.sendall(createMessage("info","Disconnecting...",False))
+                elif Letter["data"].upper() == 'Q':
+                    self.client.sendall(createMessage("info", "Thank you for using our service. Disconnecting...", False))
                     self.client.close()
                     break
                 else:
-                    self.client.sendall(createMessage("info", "invalid Input", False))
+                    self.client.sendall(createMessage("info", "Invalid selection. Please enter L, R, or Q.", False))
                     continue
         except ConnectionError:
             self.client.close()
             print(f"Client{self.addr} Disconnected")
     def Login(self):
-        self.client.sendall(createMessage("info","Username:",True))
+        self.client.sendall(createMessage("info", "Username:", True))
         username = decodeMessage(self.client.recv(1024))["data"]
         self.client.sendall(createMessage("info", "Password:", True))
         password = decodeMessage(self.client.recv(1024))["data"]
-        if not self.dataBase.searchUserPassword(username,password)[0]:
-            self.client.sendall(createMessage("info", "Wrong Username or Password",False))
+        if not self.dataBase.searchUserPassword(username, password)[0]:
+            self.client.sendall(createMessage("info", "Authentication failed: Invalid username or password", False))
             return False
         else:
-            self.client.sendall(createMessage("info","Login Success", False))
+            self.client.sendall(createMessage("info", "Authentication successful. Welcome, " + username + "!", False))
             self.isLogin = True
             self.username = username
-            LoginUsers.update_Online_LoginUsers(username,self.client,self.isLogin)
+            LoginUsers.update_Online_LoginUsers(username, self.client, self.isLogin)
             return True
-
-
-        ...
     def Register(self):
         while True:
-            self.client.sendall(createMessage("info", "Username:", True))
+            self.client.sendall(createMessage("info", "Create Username:", True))
             username = decodeMessage(self.client.recv(1024))["data"]
             if not self.dataBase.searchUser(username):
                 break
-            self.client.sendall(createMessage("info", "Username already Exists",False))
+            self.client.sendall(createMessage("info", "This username is already taken. Please choose another one.", False))
             
-        password   = None 
+        password = None 
         while True:
-            self.client.sendall(createMessage("info", "PASSWORD:", True))
+            self.client.sendall(createMessage("info", "Create Password:", True))
             password = decodeMessage(self.client.recv(1024))["data"]
-            self.client.sendall(createMessage("info", "RE-ENTER PASSWORD:", True))
+            self.client.sendall(createMessage("info", "Confirm Password:", True))
             if password == decodeMessage(self.client.recv(1024))["data"]:
-                self.client.sendall(createMessage("info", "Register Success",False))
+                self.client.sendall(createMessage("info", "Account created successfully! You can now login with your credentials.", False))
                 try:
                     self.dataBase.addUser(username, password)
-                except :
-                    self.client.sendall("Username cant be blank")
+                except:
+                    self.client.sendall("Error: Username cannot be blank.")
                     break
                 break
-            self.client.sendall(createMessage("info","Password not the same",False))
+            self.client.sendall(createMessage("info", "Passwords do not match. Please try again.", False))
